4090c00e599dd355ad5a6e3befd77b2553b37604
Tiago Rodrigues <68871083+tiago-21@users.noreply.github.com>
2024-01-28 18:41:00 -0300

    build: integração da API Via Cep

--- script.js
+++ script.js
@@ -13,3 +13,50 @@ function hideForm() {
     form.style.left = '-350px'    
     background.style.visibility = 'hidden'
 }
+
+function limpar() {
+    document.getElementById('cidade').value=("");
+    document.getElementById('uf').value=("");
+    document.getElementById('bairro').value=("");
+    document.getElementById('rua').value=("");
+}
+
+function chamada(conteudo){
+    if(!("erro" in conteudo)) {
+        document.getElementById("cidade").value=(conteudo.localidade);
+        document.getElementById("uf").value=(conteudo.uf);
+        document.getElementById("bairro").value=(conteudo.bairro);
+        document.getElementById("rua").value=(conteudo.logradouro);
+    }
+    else 
+        limpar();
+        alert("CEP não encontrado!");
+}
+
+function buscarCEP(valor) {
+    var cep = valor.replace(/\D/g, '');
+
+    if(cep != ""){
+        var validacep = /^[0-9]{8}$/;
+
+        if(validacep.test(cep)) {
+            document.getElementById('rua').value = "...";
+            document.getElementById('bairro').value = "...";
+            document.getElementById('cidade').value = "...";
+            document.getElementById('uf').value = "...";
+
+            var script = document.createElement('script');
+
+            script.src = 'https://viacep.com.br/ws/'+ cep + '/json/?callback=chamada';
+
+            document.body.appendChild(script);
+        }
+        else {
+            limpar();
+            alert("Formato de CEP inválido");
+        }
+    }
+    else {
+        limpar();
+    }
+}
