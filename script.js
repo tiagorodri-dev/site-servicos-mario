17b9b848afa4c0f425e72777be5d98d80d9870eb
Tiago Rodrigues <68871083+tiago-21@users.noreply.github.com>
2024-01-30 02:02:27 -0300

    refactor: script.js

--- script.js
+++ script.js
@@ -1,28 +1,37 @@
-const form = document.querySelector('.fale-conosco')
-const background = document.querySelector('.mascara-form')
-
-// função que traz visibilidade ao formulário
-function showForm() {
-    form.style.left = '50%'
-    form.style.transform = 'translateX(-50%)'
-    background.style.visibility = 'visible'
-}
-
-// função que deixa o formulário invisível
-function hideForm() {
-    form.style.left = '-350px'    
-    background.style.visibility = 'hidden'
+function bot() {
+    alert("Botão clicado")
 }
 
 
 // Consumo da API Via Cep
 
-// função para limpar valores do input
-function limpar() {
-    document.getElementById('cidade').value=("");
-    document.getElementById('uf').value=("");
-    document.getElementById('bairro').value=("");
-    document.getElementById('rua').value=("");
+// função de busca dos dados
+function buscarCEP(valor) {
+    let cep = valor.replace(/\D/g, '');
+
+    if(cep != ""){
+        let validacep = /^[0-9]{8}$/;
+
+        if(validacep.test(cep)) {
+            document.getElementById('cidade').value = "...";
+            document.getElementById('uf').value = "...";
+            document.getElementById('bairro').value = "...";
+            document.getElementById('rua').value = "...";
+
+            let script = document.createElement('script');
+
+            script.src = 'https://viacep.com.br/ws/'+ cep + '/json/?callback=chamada';
+
+            document.body.appendChild(script);
+        }
+        else {
+            limpar();
+            alert("Formato de CEP inválido");
+        }
+    }
+    else {
+        limpar();
+    }
 }
 
 // função para preencher os campos com os dados capturados da API
@@ -39,31 +48,10 @@ function chamada(conteudo){
     }
 }
 
-// função de busca dos dados
-function buscarCEP(valor) {
-    var cep = valor.replace(/\D/g, '');
-
-    if(cep != ""){
-        var validacep = /^[0-9]{8}$/;
-
-        if(validacep.test(cep)) {
-            document.getElementById('cidade').value = "...";
-            document.getElementById('uf').value = "...";
-            document.getElementById('bairro').value = "...";
-            document.getElementById('rua').value = "...";
-
-            var script = document.createElement('script');
-
-            script.src = 'https://viacep.com.br/ws/'+ cep + '/json/?callback=chamada';
-
-            document.body.appendChild(script);
-        }
-        else {
-            limpar();
-            alert("Formato de CEP inválido");
-        }
-    }
-    else {
-        limpar();
-    }
+// função para limpar valores do input
+function limpar() {
+    document.getElementById('cidade').value=("");
+    document.getElementById('uf').value=("");
+    document.getElementById('bairro').value=("");
+    document.getElementById('rua').value=("");
 }
